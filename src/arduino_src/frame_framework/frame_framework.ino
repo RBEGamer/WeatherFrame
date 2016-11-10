0157b735edf22ab721f73292c1725d139b306113
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2016-11-10 18:25:25 +0100

    added different pins for esp

--- src/arduino_src/frame_framework/frame_framework.ino
+++ src/arduino_src/frame_framework/frame_framework.ino
@@ -8,6 +8,12 @@
 #include <SpiRAM.h>
 #include "SdFat.h"
 #include "FatLib\FatFile.h"
+//SYSTEM DEFINES
+#define _PLATTFORM_ESP_ //ARE THIS SKETCH IS RUNNING ON A EPS ? 
+#ifdef _PLATTFORM_ESP_
+#define F(param) param
+#endif
+//#define byte unsigned char;
 
 
 //OWM SETTINGS
@@ -17,9 +23,15 @@
 #define ESP_WLAN_SSID "test_wifi"
 #define ESP_WLAN_KEY "6226054527192856"
 //PINS
-#define WS2812_PIN 8  //SETUP THE LED OUT
-#define SD_CARD_CS_PIN 4
-#define SRAM_CS_PIN 10
+#ifdef _PLATTFORM_ESP_
+#define WS2812_PIN D16
+#define SD_CARD_CS_PIN D5
+#define SRAM_CS_PIN D4
+#else //ARDUINO PINS
+#define WS2812_PIN D8
+#define SD_CARD_CS_PIN D4
+#define SRAM_CS_PIN D10
+#endif
 //DEFINE YOUR MATRIX SIZE HERE
 #define VISIBLE_MATRIX_WITH 8 //WIDTH
 #define VISIBLE_MATRIX_HEIGHT 8 //HEIGHT
@@ -70,8 +82,6 @@ const int led_id_lookup[LED_COUNT] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
 const int led_id_lookup[LED_COUNT] PROGMEM = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254 };
 #endif
 #endif
-
-
 
 //COLOR STRUCT
 #ifndef F_FRM_COLOR
@@ -512,6 +522,14 @@ void loop(){
 
 		   generate_output_layer();
 		   show_output_layer();
+		   
+
+#ifdef _PLATTFORM_ESP_
+		   yield(); //to hold wifi stable
+#endif
+
+
+
 		   delay(5000);
 
 }
